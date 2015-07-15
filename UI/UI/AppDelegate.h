297a37d8147129ba682d557eadf0b40fb133dd9f
Suxi <suxi@WeixiMac.local>
2015-07-15 10:09:46 +0800

    MyUI

---
+++ UI/UI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI
+//
+//  Created by WeixiYu on 15/7/15.
+//  Copyright (c) 2015年 liveabean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UI/UI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UI
+//
+//  Created by WeixiYu on 15/7/15.
+//  Copyright (c) 2015年 liveabean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
