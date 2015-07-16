1ec40389d3288298d48759825497ec87085f6f29
Suxi <suxi@WeixiMac.local>
2015-07-16 15:20:19 +0800

    增加了搜索结果的页面 tableVIewCell

---
+++ UI/UI/CZSearchResult.m
@@ -0,0 +1,13 @@
+//
+//  CZSearchResult.m
+//  UI
+//
+//  Created by WeixiYu on 15/7/16.
+//  Copyright (c) 2015年 liveabean. All rights reserved.
+//
+
+#import "CZSearchResult.h"
+
+@implementation CZSearchResult
+
+@end
--- UI/UI/SearchResultsViewController.m
+++ UI/UI/SearchResultsViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "SearchResultsViewController.h"
+#import "CZSearchResult.h"
 
-@interface SearchResultsViewController ()
+@interface SearchResultsViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableVIew;
 
 @end
 
@@ -16,12 +18,92 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+//Set tableView
+//Sections
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 3;
+}
+//Sections' rows
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (section==0) {
+        return 3;
+    }else if(section==1) {
+        return 2;
+    }else {
+        return 1;
+    }
+}
+//TableViewCell
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:nil];
+   
+    //section 0
+    if (indexPath.section==0) {
+        if (indexPath.row==0) {
+            cell.textLabel.text=@"荔园超市";
+            cell.detailTextLabel.text=@"2014.10.12";
+            cell.imageView.image=[UIImage imageNamed:@"174074057446.png"];
+        } else if(indexPath.row==1){
+            cell.textLabel.text=@"南区超市";
+            cell.detailTextLabel.text=@"2014.10.12";
+            cell.imageView.image=[UIImage imageNamed:@"173890450691.png"];
+        } else{
+            cell.textLabel.text=@"南区超市";
+            cell.imageView.image=[UIImage imageNamed:@"173890255948.png"];
+            cell.detailTextLabel.text=@"2014.10.12";
+        }
+    //section 1
+    } else if(indexPath.section==1) {
+        if (indexPath.row==0) {
+            cell.textLabel.text=@"荔园超市";
+            cell.detailTextLabel.text=@"2014.11.12";
+        } else if(indexPath.row==1){
+            cell.textLabel.text=@"南区超市";
+            cell.detailTextLabel.text=@"2014.11.12";
+        } else{
+            cell.textLabel.text=@"南区大酒店";
+            cell.detailTextLabel.text=@"2014.11.12";
+        }
+    }
+    //section2
+    else {
+        if (indexPath.row==0) {
+            cell.textLabel.text=@"荔园超市";
+            cell.detailTextLabel.text=@"2014.12.12";
+        } else if(indexPath.row==1){
+            cell.textLabel.text=@"南区超市";
+            cell.detailTextLabel.text=@"2014.12.12";
+        } else{
+            cell.textLabel.text=@"南区大酒店";
+            cell.detailTextLabel.text=@"2014.12.12";
+        }
+    }
+    
+    return cell;
+}
+
+//分组头标题
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    if (section==0) {
+        return @"10月";
+    } else if(section==1){
+        return  @"11月";
+    } else {
+        return @"12月";
+    }
 }
 
 
